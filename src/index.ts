d0ff656cc26a9551e183bc95b14e4e14258b4ec9
Urip Subekti <urip.subekti13@gmail.com>
2021-03-28 18:56:23 +0700

    remove warning

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,46 @@
-import { Button, Input, Padder, Col, Row, Gap, ScaledText, Dropdown, RadioButton, Radio, RadioButtonGroup } from "./component";
+import { LogBox } from 'react-native'
+import {
+ Button,
+ Input,
+ Padder,
+ Col,
+ Row,
+ Gap,
+ ScaledText,
+ Dropdown,
+ RadioButton,
+ Radio,
+ RadioButtonGroup,
+} from "./component";
 import * as _ from "lodash";
 import * as mmt from "moment";
 import * as sizeMatters from "react-native-size-matters";
 import { fromIDR, formatIDR, formatDate, StorageUtil } from "./util";
 
+
+LogBox.ignoreLogs(["Require cycle:"]);
+
 const utils = {
-  _,
-  mmt,
-  sizeMatters,
-  fromIDR,
-  formatIDR,
-  formatDate,
-  storage: new StorageUtil()
+ _,
+ mmt,
+ sizeMatters,
+ fromIDR,
+ formatIDR,
+ formatDate,
+ storage: new StorageUtil(),
 };
 
 export {
-  Row,
-  Col,
-  Button,
-  Padder,
-  Input,
-  Gap,
-  ScaledText,
-  Dropdown,
-  Radio, 
-  RadioButton,
-  RadioButtonGroup,
-  utils
+ Row,
+ Col,
+ Button,
+ Padder,
+ Input,
+ Gap,
+ ScaledText,
+ Dropdown,
+ Radio,
+ RadioButton,
+ RadioButtonGroup,
+ utils,
 };
-
-
-
