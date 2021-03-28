6a28502f42065d79584c2077337493d0816636f8
Urip Subekti <urip.subekti13@gmail.com>
2021-03-28 18:05:51 +0700

    add postinstall

--- src/util/storage.util.ts
+++ src/util/storage.util.ts
@@ -1,36 +1,34 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AsyncStorage } from "react-native";
 
 export class StorageUtil {
-  private _data = {};
-  constructor() {
-    this.init();
-  }
+ private _data = {};
+ constructor() {
+  this.init();
+ }
 
-  public async init() {
-    const keys = (await AsyncStorage.getAllKeys()) || [];
-    const promises = keys.map((key) => AsyncStorage.getItem(key));
-    const data = await Promise.all(promises);
-    const result = {};
-    for (const key of keys) {
-      let value = null;
-      try {
-        value = JSON.parse(data[key]);
-      } catch (error) {
-        value = data[key];
-      }
-      result[key] = value;
-    }
-    this._data = result;
+ public async init() {
+  const keys = (await AsyncStorage.getAllKeys()) || [];
+  const promises = keys.map((key) => AsyncStorage.getItem(key));
+  const data: any = await Promise.all(promises);
+  const result = {};
+  for (const key of keys) {
+   let value = null;
+   try {
+    value = JSON.parse(data[key]);
+   } catch (error) {
+    value = data[key];
+   }
+   result[key] = value;
   }
+  this._data = result;
+ }
 
-  public get(key: string) {
-    return this._data[key];
-  }
+ public get(key: string) {
+  return this._data[key];
+ }
 
-  public set(key: string, value: string | object) {
-    if (typeof value === "object") value = JSON.stringify(value);
-    AsyncStorage.setItem(key, value);
-  }
+ public set(key: string, value: string | object) {
+  if (typeof value === "object") value = JSON.stringify(value);
+  AsyncStorage.setItem(key, value);
+ }
 }
-
-
