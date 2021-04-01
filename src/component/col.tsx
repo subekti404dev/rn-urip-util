ca8b3e37e384a91cf4f76760c9c1a08d4eb87710
Urip Subekti <urip.subekti13@gmail.com>
2021-04-01 15:08:47 +0700

    fix col row

--- src/component/col.tsx
+++ src/component/col.tsx
@@ -1,41 +1,44 @@
 import * as React from "react";
-import { View, StyleProp, ViewStyle } from "react-native";
+import { View, StyleProp, ViewStyle, TouchableOpacity } from "react-native";
 
 interface ColProps {
-  children?: any;
-  size?: number;
-  style?: StyleProp<ViewStyle>;
-  justifyStart?: boolean;
-  justifyCenter?: boolean;
-  justifyEnd?: boolean;
-  alignStart?: boolean;
-  alignCenter?: boolean;
-  alignEnd?: boolean;
-  center?: boolean;
-  color?: string;
+ children?: any;
+ size?: number;
+ style?: StyleProp<ViewStyle>;
+ justifyStart?: boolean;
+ justifyCenter?: boolean;
+ justifyEnd?: boolean;
+ alignStart?: boolean;
+ alignCenter?: boolean;
+ alignEnd?: boolean;
+ center?: boolean;
+ color?: string;
+ onPress?: () => void;
 }
 
- const Col = (props: ColProps) => {
-  const flex = props.size || 1;
-  const style: any = props.style || {};
-  style.flex = flex;
-  style.flexDirection = 'column';
-  if (props.justifyStart) style.justifyContent = 'flext-start';
-  if (props.justifyCenter) style.justifyContent = 'center';
-  if (props.justifyEnd) style.justifyContent = 'flex-end';
-  if (props.alignStart) style.alignItems = 'flex-start';
-  if (props.alignCenter) style.alignItems = 'center';
-  if (props.alignEnd) style.alignItems = 'flex-end';
-  if (props.color) style.backgroundColor = props.color;
-  if (props.center) {
-    style.alignItems = 'center';
-    style.justifyContent = 'center';
-  }
-  return (
-    <View style={style}>
-      {props.children}
-    </View>
-  );
+const Col = (props: ColProps) => {
+ const flex = props.size || 1;
+ const style: any = props.style || {};
+ style.flex = flex;
+ style.flexDirection = "column";
+ if (props.justifyStart) style.justifyContent = "flext-start";
+ if (props.justifyCenter) style.justifyContent = "center";
+ if (props.justifyEnd) style.justifyContent = "flex-end";
+ if (props.alignStart) style.alignItems = "flex-start";
+ if (props.alignCenter) style.alignItems = "center";
+ if (props.alignEnd) style.alignItems = "flex-end";
+ if (props.color) style.backgroundColor = props.color;
+ if (props.center) {
+  style.alignItems = "center";
+  style.justifyContent = "center";
+ }
+
+ const Component = props.onPress ? TouchableOpacity : View;
+ return (
+  <Component onPress={props.onPress} style={style}>
+   {props.children}
+  </Component>
+ );
 };
 
 export default Col;
--- src/component/row.tsx
+++ src/component/row.tsx
@@ -1,47 +1,50 @@
 import * as React from "react";
-import { View, StyleProp, ViewStyle } from "react-native";
+import { View, StyleProp, ViewStyle, TouchableOpacity } from "react-native";
 import { scale } from "react-native-size-matters";
 
 interface RowProps {
-  children?: any;
-  size?: number;
-  style?: StyleProp<ViewStyle>;
-  justifyStart?: boolean;
-  justifyCenter?: boolean;
-  justifyEnd?: boolean;
-  alignStart?: boolean;
-  alignCenter?: boolean;
-  alignEnd?: boolean;
-  center?: boolean;
-  color?: string;
-  height?: number;
+ children?: any;
+ size?: number;
+ style?: StyleProp<ViewStyle>;
+ justifyStart?: boolean;
+ justifyCenter?: boolean;
+ justifyEnd?: boolean;
+ alignStart?: boolean;
+ alignCenter?: boolean;
+ alignEnd?: boolean;
+ center?: boolean;
+ color?: string;
+ height?: number;
+ onPress?: () => void;
 }
 
 const Row = (props: RowProps) => {
-  const flex = props.size || 1;
-  const style: any = props.style || {};
-  style.flex = flex;
-  style.flexDirection = 'row';
-  if (props.justifyStart) style.justifyContent = 'flext-start';
-  if (props.justifyCenter) style.justifyContent = 'center';
-  if (props.justifyEnd) style.justifyContent = 'flex-end';
-  if (props.alignStart) style.alignItems = 'flex-start';
-  if (props.alignCenter) style.alignItems = 'center';
-  if (props.alignEnd) style.alignItems = 'flex-end';
-  if (props.color) style.backgroundColor = props.color;
-  if (props.center) {
-    style.alignItems = 'center';
-    style.justifyContent = 'center';
-  }
-  if (props.height) {
-    style.height = scale(props.height);
-    delete style.flex;
-  }
-  return (
-    <View style={style}>
-      {props.children}
-    </View>
-  );
+ const flex = props.size || 1;
+ const style: any = props.style || {};
+ style.flex = flex;
+ style.flexDirection = "row";
+ if (props.justifyStart) style.justifyContent = "flext-start";
+ if (props.justifyCenter) style.justifyContent = "center";
+ if (props.justifyEnd) style.justifyContent = "flex-end";
+ if (props.alignStart) style.alignItems = "flex-start";
+ if (props.alignCenter) style.alignItems = "center";
+ if (props.alignEnd) style.alignItems = "flex-end";
+ if (props.color) style.backgroundColor = props.color;
+ if (props.center) {
+  style.alignItems = "center";
+  style.justifyContent = "center";
+ }
+ if (props.height) {
+  style.height = scale(props.height);
+  delete style.flex;
+ }
+ const Component = props.onPress ? TouchableOpacity : View;
+
+ return (
+  <Component onPress={props.onPress} style={style}>
+   {props.children}
+  </Component>
+ );
 };
 
 export default Row;
